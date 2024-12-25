779c635bbe91ecc4da0fa30bf64f6f5591790a94
Luke Kuzmish <lakuzmish@gmail.com>
2024-12-25 10:37:12 -0500

    formatting

--- day06/index.ts
+++ day06/index.ts
@@ -1,54 +1,58 @@
-const FILENAME = 'input.txt';
+const FILENAME = "input.txt";
 
 enum Direction {
-    UP,
-    RIGHT,
-    DOWN,
-    LEFT
+  UP,
+  RIGHT,
+  DOWN,
+  LEFT,
 }
 
 interface GameState {
-    personLocation: Location,
-    personDirection: Direction,
-    grid: ('#' | undefined)[][],
-    spacesMoved: Set<string>,
+  personLocation: Location;
+  personDirection: Direction;
+  grid: ("#" | undefined)[][];
+  spacesMoved: Set<string>;
 }
 
 interface Location {
-    readonly x: number,
-    readonly y: number
-};
+  readonly x: number;
+  readonly y: number;
+}
 
 const convertToGrid = (str: string): GameState => {
-    let grid: ('#' | undefined)[][] = [];
-    let personLocation;
+  let grid: ("#" | undefined)[][] = [];
+  let personLocation;
 
-    const lines = str.split("\n");
-    const WIDTH = lines[0].length;
+  const lines = str.split("\n");
+  const WIDTH = lines[0].length;
 
-    for (let y = 0; y < lines.length; y++) {
-        let line = lines[y];
-        let currentRow: ('#' | undefined)[] = Array(WIDTH).fill(undefined);
-        for (let x = 0; x < WIDTH; x++) {
-            let char = line.charAt(x);
-            if (char === '.') {
-                continue;
-            }
-            if (char === '#') {
-                currentRow[x] = '#';
-            }
-            else {
-                personLocation = { x, y }
-            }
-        }
-        grid.push(currentRow);
+  for (let y = 0; y < lines.length; y++) {
+    let line = lines[y];
+    let currentRow: ("#" | undefined)[] = Array(WIDTH).fill(undefined);
+    for (let x = 0; x < WIDTH; x++) {
+      let char = line.charAt(x);
+      if (char === ".") {
+        continue;
+      }
+      if (char === "#") {
+        currentRow[x] = "#";
+      } else {
+        personLocation = { x, y };
+      }
     }
+    grid.push(currentRow);
+  }
 
-    if (personLocation === undefined) {
-        throw new Error("Could not find person!");
-    }
+  if (personLocation === undefined) {
+    throw new Error("Could not find person!");
+  }
 
-    return { personLocation: personLocation, personDirection: Direction.UP, grid: grid, spacesMoved: new Set() };
+  return {
+    personLocation: personLocation,
+    personDirection: Direction.UP,
+    grid: grid,
+    spacesMoved: new Set(),
+  };
 };
 
 const file = Bun.file(FILENAME);
@@ -59,59 +63,61 @@ findPath(gamestate);
 console.log(gamestate.spacesMoved.size);
 
 function findPath(gamestate: GameState) {
-    const [dx, dy] = getDxDy(gamestate.personDirection);
+  const [dx, dy] = getDxDy(gamestate.personDirection);
 
-    gamestate.spacesMoved.add(`${gamestate.personLocation.x}__${gamestate.personLocation.y}`)
-    let [nextX, nextY] = [gamestate.personLocation.x + dx, gamestate.personLocation.y + dy];
-    // check if within bounds board bounds
-    if (nextX >= gamestate.grid[0].length || nextX === -1) {
-        // we won! let's bail
-        return;
-    }
-    if (nextY >= gamestate.grid.length || nextY === -1) {
-        return;
-    }
+  gamestate.spacesMoved.add(
+    `${gamestate.personLocation.x}__${gamestate.personLocation.y}`,
+  );
+  let [nextX, nextY] = [
+    gamestate.personLocation.x + dx,
+    gamestate.personLocation.y + dy,
+  ];
+  // check if within bounds board bounds
+  if (nextX >= gamestate.grid[0].length || nextX === -1) {
+    // we won! let's bail
+    return;
+  }
+  if (nextY >= gamestate.grid.length || nextY === -1) {
+    return;
+  }
 
-    if (gamestate.grid[nextY][nextX] === undefined) {
-        gamestate.personLocation = { x: nextX, y: nextY };
-    }
-    else {
-        gamestate.personDirection = getNextDirection(gamestate.personDirection);
-
-    }
-    findPath(gamestate);
-
+  if (gamestate.grid[nextY][nextX] === undefined) {
+    gamestate.personLocation = { x: nextX, y: nextY };
+  } else {
+    gamestate.personDirection = getNextDirection(gamestate.personDirection);
+  }
+  findPath(gamestate);
 }
 
 function getNextDirection(direction: Direction): Direction {
-    switch (direction) {
-        case Direction.UP:
-            return Direction.RIGHT;
-        case Direction.RIGHT:
-            return Direction.DOWN;
-        case Direction.DOWN:
-            return Direction.LEFT;
-        case Direction.LEFT:
-            return Direction.UP;
-    }
+  switch (direction) {
+    case Direction.UP:
+      return Direction.RIGHT;
+    case Direction.RIGHT:
+      return Direction.DOWN;
+    case Direction.DOWN:
+      return Direction.LEFT;
+    case Direction.LEFT:
+      return Direction.UP;
+  }
 }
 
 function getDxDy(direction: Direction): [number, number] {
-    let dx = 0,
-        dy = 0;
-    switch (gamestate.personDirection) {
-        case Direction.UP:
-            dy = -1;
-            break;
-        case Direction.DOWN:
-            dy = 1;
-            break;
-        case Direction.LEFT:
-            dx = -1;
-            break;
-        case Direction.RIGHT:
-            dx = 1;
-            break;
-    }
-    return [dx, dy];
+  let dx = 0,
+    dy = 0;
+  switch (gamestate.personDirection) {
+    case Direction.UP:
+      dy = -1;
+      break;
+    case Direction.DOWN:
+      dy = 1;
+      break;
+    case Direction.LEFT:
+      dx = -1;
+      break;
+    case Direction.RIGHT:
+      dx = 1;
+      break;
+  }
+  return [dx, dy];
 }
